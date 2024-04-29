1218f44cc36a2167610e630cd98002b8e2eba07f
Vincent Walker <vwal290@aucklanduni.ac.nz>
2024-04-29 21:30:34 +1200

    added template files to begin creation of landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add your JavaScript code here
+// This is a template script file
+
+// Example function
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+// Call the function
+greet("World");
