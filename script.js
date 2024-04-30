bfd2ab0d48682e5f256be1769886535617d6d6eb
VINCENT <vwal290@aucklanduni.ac.nz>
2024-04-30 12:25:42 +1200

    improved registration form, made it modal popup

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-// Add your JavaScript code here
-// This is a template script file
+// Get the modal
+var modal = document.getElementById("modal");
 
-// Example function
-function greet(name) {
-    console.log(`Hello, ${name}!`);
+// Get the button that opens the modal
+var triggerModal = document.getElementById("trigger-modal");
+
+// Get the close button
+var closeModal = document.getElementById("close-modal");
+
+// When the user clicks the button, open the modal
+triggerModal.onclick = function () {
+    modal.style.display = "block";
 }
 
-// Call the function
-greet("World");
+// When the user clicks on <span> (x), close the modal
+closeModal.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
